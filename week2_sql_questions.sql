bf5efe3abe2d79a1dc4d5a550756e090e70ed90f
phillip-ernest <phillipernest08@gmail.com>
2026-09-24 10:52:27 +0200

    Add SQL assignment queries

---
+++ week2_sql_questions.sql
@@ -0,0 +1,29 @@
+SHOW DATABASES;
+USE sales;
+SELECT
+    paymentDate,
+    SUM(amount) AS total_amount_paid
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+SELECT
+    customerName,
+    country,
+    AVG(creditLimit) AS average_credit_limit
+FROM customers
+GROUP BY customerName, country;
+SELECT
+    productCode,
+    quantityOrdered,
+    SUM(quantityOrdered * priceEach) AS total_price
+FROM orderdetails
+GROUP BY productCode, quantityOrdered;
+
+SELECT
+    checkNumber,
+    MAX(amount) AS highest_amount_paid
+FROM payments
+GROUP BY checkNumber;
+
+     
